7a0f6014fbb81370cce2d7fe43334b5ddeab46e0
pelleservan <pelle.servan@hotmail.fr>
2025-11-23 21:31:10 +0100

    init decap csm config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import {NextConfig} from 'next';
+import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
- 
-const nextConfig: NextConfig = {};
- 
+
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/admin',
+        destination: '/admin/index.html', // sert le fichier statique directement
+      },
+    ]
+  },
+};
+
 const withNextIntl = createNextIntlPlugin('./app/i18n/request.ts');
+
 export default withNextIntl(nextConfig);
